fb257940d788d5dfd515263cf70f67f78cec6f62
Aryan Singh <aryansingh920@outlook.com>
2026-02-27 23:03:12 +0530

    add hello and sum programs with basic functionality

---
+++ src/main/hello.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // int a = 10;
+    // long long b = 9999999999999999999LL;
+    // double d = 22.0 / 7.0;
+    // bool flag = true;
+    // flag = false;
+    // char ch = 'A';
+    // auto x = 10;
+    // cout << "Hello World!" << endl;
+    // cout << b << "," << d << x << endl;
+    // int xy;
+    // cin >> xy;
+    // cout << xy+a+d << endl;
+    // printf("Hi\n");
+    // string s;
+    // cout << "Enter name : ";
+    // cin >> s;
+    // cout << "Hi " << s;
+    // int age;
+    // scanf("%d", &age);
+    // printf("Hi %d\n", age);
+    // if (10 > 5){
+    // }else if (10 > 5)
+    // {
+    //     /* code */
+    // }else{
+    // }
+    // for (int i = 1; i <= 10;i++){
+    //     cout << i << ", ";
+    // }
+    // cout << endl;
+    // while (condition)
+    // {
+    //     /* code */
+    // }
+    // do
+    // {
+    //     /* code */
+    // } while (10);
+    // vector<int> v = {1, 2, 3, 4, 5};
+    // for(int num : v){
+    //     cout << num << ", ";
+    // }
+
+
+    
+    cout << endl;
+    return 0;
+}
