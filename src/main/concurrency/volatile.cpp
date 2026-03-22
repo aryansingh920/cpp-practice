1258d9fab0638f7da5fc8d70831377c8c2a4eb21
Aryan Singh <aryansingh920@outlook.com>
2026-03-22 14:43:44 +0530

    add: implement basic example of using volatile in concurrency

---
+++ src/main/concurrency/volatile.cpp
@@ -0,0 +1,10 @@
+#include "../DSA_Master.h"
+
+int main(){
+    array<int,5> a = {5,6,3,1,7};
+    for(int x:a){
+        cout<<x<<" , ";
+    }
+    volatile bool buttonPressed = false;
+    return 0;
+}
